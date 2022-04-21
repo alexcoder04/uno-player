bf847eda327ebcb72e9e97badf65d7595ec6dda1
alexcoder04 <alexcoder04@protonmail.com>
2022-04-21 19:55:01 +0200

    add image sorter

---
+++ data/sort.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+colors = ["b", "r", "y", "g", "j"]
+numbers = [str(i) for i in range(1, 9)] + ["n", "r", "+2", "+4", "j"]
+
+def run_color():
+    for c in colors:
+        os.mkdir("./colors/" + c)
+        for i in os.listdir("./raw"):
+            if i.startswith(c):
+                os.system(f"cp -v ./raw/{i} ./colors/{c}/{i[1:]}")
+
+def run_number():
+    for n in numbers:
+        os.mkdir("./numbers/" + n)
+        for i in os.listdir("./raw"):
+            if i[1:].startswith(n):
+                os.system(f"cp -v ./raw/{i} ./numbers/{n}/{i[0]}{i.split('-')[1]}")
+
+try:
+    os.mkdir("./colors")
+    os.mkdir("./numbers")
+except FileExistsError:
+    print("./sorted already exists")
+
+print(colors)
+print(numbers)
+
+if "color" in sys.argv:
+    run_color()
+
+if "number" in sys.argv:
+    run_number()
+
