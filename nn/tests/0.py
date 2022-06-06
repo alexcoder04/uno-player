5f8f6134c42de612a6f2d63678aec3f855fa7d10
alexcoder04 <alexcoder04@protonmail.com>
2022-06-06 19:18:28 +0200

    update nn/tests/0.py

--- nn/tests/0.py
+++ nn/tests/0.py
@@ -1,45 +1,73 @@
 #!/usr/bin/env python3
 
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing import image
 import tensorflow as tf
 import numpy as np
 
 base_folder = "../../data/colors/"
 
-train = ImageDataGenerator(rescale= 1/255)
-validation = ImageDataGenerator(rescale= 1/255)
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    "../../data/colors/raw",
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(32, 32),
+    batch_size=10)
 
-train_dataset = train.flow_from_directory(base_folder + "train/", target_size=(32,32),batch_size=10, class_mode="categorical")
-test_dataset = validation.flow_from_directory(base_folder + "test/", target_size=(32,32),batch_size=10, class_mode="categorical")
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    "../../data/colors/raw",
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(32, 32),
+    batch_size=10)
+
+class_names = train_ds.class_names
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(16, (3,3), activation="relu", input_shape=(32,32,3)),
-    tf.keras.layers.MaxPool2D(2, 2),
-
-    tf.keras.layers.Conv2D(32, (3,3), activation="relu"),
-    tf.keras.layers.MaxPool2D(2, 2),
-
-    tf.keras.layers.Conv2D(64, (3,3), activation="relu"),
-    tf.keras.layers.MaxPool2D(2, 2),
-
+    tf.keras.layers.Rescaling(1./255, input_shape=(32, 32, 3)),
+    tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation="relu"),
-    tf.keras.layers.Dense(1, activation="sigmoid")
-    ])
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(len(class_names))
+])
 
-model.compile(loss="binary_crossentropy", optimizer="adam" ,metrics=["accuracy"])
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
-model_fit = model.fit(train_dataset, steps_per_epoch=3, epochs=5, validation_data=test_dataset)
+epochs=5
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
 
-img = image.load_img("./colors/test/g/1-2.jpg", target_size=(32,32))
-X = image.img_to_array(img)
-X = np.expand_dims(X, axis=0)
-images = np.vstack([X])
+def test(filename):
+    img = tf.keras.preprocessing.image.load_img(filename, target_size=(32,32))
+    X = tf.keras.preprocessing.image.img_to_array(img)
+    X = np.expand_dims(X, axis=0)
+    images = np.vstack([X])
 
-val = model.predict(images)
+    val = model.predict(images)
+    max_val = -1000
+    res = 1000
+    for i, v in enumerate(val[0]):
+        if v >= max_val:
+            max_val = v
+            res = i
 
-print(val)
-print(train_dataset.class_indices)
+    print(f"{filename} => {class_names[res]}")
 
+for i in ["./b/9-8.jpg", "./g/4-9.jpg", "./b/2-10.jpg", "./r/r-6.jpg", "./g/2-10.jpg", "./b/2-1.jpg", "./r/1-10.jpg", "./y/8-7.jpg", "./y/7-6.jpg", "./y/1-10.jpg"]:
+    test("../../data/colors/raw/" + i)
 
